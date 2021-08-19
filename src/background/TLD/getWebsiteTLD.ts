05ea38583267408e723b0db06aa0f58007498b1d
Progitiel <github@progiciel.be>
2021-08-19 20:23:45 +0200

    Fix url

--- src/background/TLD/getWebsiteTLD.ts
+++ src/background/TLD/getWebsiteTLD.ts
@@ -3,11 +3,12 @@ import { parseHostname } from '../utils/url'
 import Website_be from './api/be'
 import Website_rdap from './rdap'
 
-export default function (hostname: string): Website {
+export default function (url: string): Website {
+  const { hostname } = new URL(url)
   const { tld } = parseHostname(hostname)
 
   if (tld === 'be') {
-    return new Website_be(hostname)
+    return new Website_be(url)
   }
-  return new Website_rdap(hostname)
+  return new Website_rdap(url)
 }
