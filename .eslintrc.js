c7d61ec80a5d8530b933d134a864030c905fc64c
Progitiel <github@progiciel.be>
2021-08-27 19:20:09 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    webextensions: true,
-  },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'warn',
-  },
+    root: true,
+    env: {
+        node: true,
+        webextensions: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-unused-vars': 'error',
+        '@typescript-eslint/no-unused-vars': 'error',
+    },
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+    },
 }
