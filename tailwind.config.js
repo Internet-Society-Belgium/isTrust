6a4ce5f103b6f71193b1e974395485d572f16587
Progitiel <github@progiciel.be>
2021-09-06 04:38:58 +0200

    tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
     purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: false,
     theme: {
-        extend: {},
+        colors: {
+            primary: '#0072f2',
+            secondary: '#191b1f',
+            'neutral-text': '#89939b',
+            container: '#ffffff',
+            background: '#f2f4f5',
+            loading: '#89939b',
+        },
+        fontFamily: {
+            sans: ['Rubik', 'sans-serif'],
+        },
+        extend: {
+            minWidth: {
+                40: '10rem',
+            },
+        },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
 }
