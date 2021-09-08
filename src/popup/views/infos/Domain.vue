2874bb5c9a1f5255ceda3f17686e64b7d92844c1
Progitiel <github@progiciel.be>
2021-09-08 19:49:20 +0200

    improve Domain

--- src/popup/views/infos/Domain.vue
+++ src/popup/views/infos/Domain.vue
@@ -72,55 +72,50 @@
             </div>
         </div>
 
-        <div>
-            <div class="p-1">
-                <div class="flex items-center gap-2">
-                    <OfficeBuildingIcon
-                        class="flex-none w-5 h-5"
-                        :class="[
-                            website.states?.score?.domain?.registrant ===
-                            'warning'
-                                ? 'text-warning'
-                                : 'text-neutral',
-                        ]"
-                    />
-                    <div class="flex-grow">
-                        <div class="flex items-center gap-0.5">
-                            <p>
-                                {{
-                                    website.states?.data?.dns?.registrant
-                                        ?.organisation || 'No organisation'
-                                }}
-                            </p>
-                            <div v-if="website.states.data.dns.dnssec">
-                                <BadgeCheckIcon class="w-4 h-4 text-neutral" />
-                            </div>
+        <div class="p-1">
+            <div class="flex items-center gap-2">
+                <OfficeBuildingIcon
+                    class="flex-none w-5 h-5"
+                    :class="[
+                        website.states?.score?.domain?.registrant === 'warning'
+                            ? 'text-warning'
+                            : 'text-neutral',
+                    ]"
+                />
+                <div class="flex-grow">
+                    <div class="flex items-center gap-0.5">
+                        <p>
+                            {{
+                                website.states?.data?.dns?.registrant
+                                    ?.organisation || 'No organisation'
+                            }}
+                        </p>
+                        <div v-if="website.states?.data?.dns?.dnssec">
+                            <BadgeCheckIcon class="w-4 h-4 text-neutral" />
                         </div>
                     </div>
                 </div>
-                <div
-                    v-if="website.states.data?.dns?.registrant?.location"
-                    class="flex items-center gap-2 pl-6"
-                >
-                    <LocationMarkerIcon
-                        class="flex-none w-5 h-5 text-neutral"
-                    />
-                    <div class="flex-grow">
-                        <p class="whitespace-nowrap">
-                            {{
-                                [
-                                    website.states.data?.dns?.registrant
-                                        ?.location?.state || '',
-                                    website.states.data?.dns?.registrant
-                                        ?.location?.region || '',
-                                    website.states.data?.dns?.registrant
-                                        ?.location?.country || '',
-                                ]
-                                    .filter((e) => e != '')
-                                    .join(' - ')
-                            }}
-                        </p>
-                    </div>
+            </div>
+            <div
+                v-if="website.states.data?.dns?.registrant?.location"
+                class="flex items-center gap-2 pl-6"
+            >
+                <LocationMarkerIcon class="flex-none w-5 h-5 text-neutral" />
+                <div class="flex-grow">
+                    <p class="whitespace-nowrap">
+                        {{
+                            [
+                                website.states.data?.dns?.registrant?.location
+                                    ?.state || '',
+                                website.states.data?.dns?.registrant?.location
+                                    ?.region || '',
+                                website.states.data?.dns?.registrant?.location
+                                    ?.country || '',
+                            ]
+                                .filter((e) => e != '')
+                                .join(' - ')
+                        }}
+                    </p>
                 </div>
             </div>
         </div>
