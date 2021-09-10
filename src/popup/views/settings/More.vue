ef1dc5bff164b9a7a0fb205051400acfeb262d77
Progitiel <github@progiciel.be>
2021-09-10 20:07:10 +0200

    refactor: go back to settings design

--- src/popup/views/settings/More.vue
+++ src/popup/views/settings/More.vue
@@ -1,71 +1,69 @@
 <template>
     <div class="m-2 bg-container rounded-lg">
-        <div class="grid grid-flow-row gap-2 p-3">
-            <a
-                class="flex items-center"
-                href="#"
-                @click="
-                    openUrl(
-                        'https://github.com/Internet-Society-Belgium/trest/blob/main/PRIVACY.md'
-                    )
-                "
-            >
-                <div class="flex-none">
-                    <ShieldCheckIcon class="w-6 h-6 text-neutral" />
-                </div>
-                <p class="flex-grow pl-1 pr-6 whitespace-nowrap">
-                    Privacy Policy
-                </p>
-                <div class="flex-none">
-                    <ExternalLinkIcon class="w-5 h-5 text-neutral" />
-                </div>
-            </a>
-
-            <a
-                class="flex items-center"
-                href="#"
-                @click="
-                    openUrl(
-                        'https://github.com/Internet-Society-Belgium/trest/blob/main/LICENSE.md'
-                    )
-                "
-            >
-                <div class="flex-none">
-                    <ScaleIcon class="w-6 h-6 text-neutral" />
-                </div>
-                <p class="flex-grow pl-1 pr-6 whitespace-nowrap">License</p>
-                <div class="flex-none">
-                    <ExternalLinkIcon class="w-5 h-5 text-neutral" />
-                </div>
-            </a>
-
-            <a
-                class="flex items-center"
-                href="#"
-                @click="
-                    openUrl(
-                        'https://github.com/Internet-Society-Belgium/trest/blob/main/CREDITS.md'
-                    )
-                "
-            >
-                <div class="flex-none">
-                    <UserGroupIcon class="w-6 h-6 text-neutral" />
-                </div>
-                <p class="flex-grow pl-1 pr-6 whitespace-nowrap">Credits</p>
-                <div class="flex-none">
-                    <ExternalLinkIcon class="w-5 h-5 text-neutral" />
-                </div>
-            </a>
-
+        <div class="grid grid-flow-row gap-4 p-3">
             <router-link to="/settings" tag="div" class="flex items-center">
-                <div class="flex-none">
-                    <AdjustmentsIcon class="w-6 h-6 text-neutral" />
-                </div>
-                <p class="flex-grow pl-1 pr-6 whitespace-nowrap">Settings</p>
                 <div class="flex-none">
                     <ChevronLeftIcon class="w-5 h-5 text-neutral" />
                 </div>
+                <p class="flex-grow pl-1 pr-6 whitespace-nowrap">Settings</p>
             </router-link>
+            <div class="grid grid-flow-row gap-2">
+                <a
+                    class="flex items-center"
+                    href="#"
+                    @click="
+                        openUrl(
+                            'https://github.com/Internet-Society-Belgium/trest/blob/main/PRIVACY.md'
+                        )
+                    "
+                >
+                    <div class="flex-none">
+                        <ShieldCheckIcon class="w-6 h-6 text-neutral" />
+                    </div>
+                    <p class="flex-grow pl-1 pr-6 whitespace-nowrap">
+                        Privacy Policy
+                    </p>
+                    <div class="flex-none">
+                        <ExternalLinkIcon class="w-5 h-5 text-neutral" />
+                    </div>
+                </a>
+
+                <a
+                    class="flex items-center"
+                    href="#"
+                    @click="
+                        openUrl(
+                            'https://github.com/Internet-Society-Belgium/trest/blob/main/LICENSE.md'
+                        )
+                    "
+                >
+                    <div class="flex-none">
+                        <ScaleIcon class="w-6 h-6 text-neutral" />
+                    </div>
+                    <p class="flex-grow pl-1 pr-6 whitespace-nowrap">License</p>
+                    <div class="flex-none">
+                        <ExternalLinkIcon class="w-5 h-5 text-neutral" />
+                    </div>
+                </a>
+
+                <a
+                    class="flex items-center"
+                    href="#"
+                    @click="
+                        openUrl(
+                            'https://github.com/Internet-Society-Belgium/trest/blob/main/CREDITS.md'
+                        )
+                    "
+                >
+                    <div class="flex-none">
+                        <UserGroupIcon class="w-6 h-6 text-neutral" />
+                    </div>
+                    <p class="flex-grow pl-1 pr-6 whitespace-nowrap">Credits</p>
+                    <div class="flex-none">
+                        <ExternalLinkIcon class="w-5 h-5 text-neutral" />
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </template>
@@ -76,7 +74,6 @@
         ShieldCheckIcon,
         UserGroupIcon,
         ScaleIcon,
-        AdjustmentsIcon,
         ChevronLeftIcon,
     } from '@heroicons/vue/outline'
     import { defineComponent, inject } from 'vue'
@@ -89,7 +86,6 @@
             ShieldCheckIcon,
             UserGroupIcon,
             ScaleIcon,
-            AdjustmentsIcon,
             ChevronLeftIcon,
         },
         setup() {
