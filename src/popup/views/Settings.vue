d8e6ff36767c710af1059e4a1b3dd603de0bb737
Progitiel <github@progiciel.be>
2021-09-10 03:59:08 +0200

    feat: settings
    Privacy Policy
    License
    Credits
    
    #4

--- src/popup/views/Settings.vue
+++ src/popup/views/Settings.vue
@@ -1,23 +1,81 @@
 <template>
-    <div class="">
-        <div>{{ JSON.stringify(settings, null, 2) }}</div>
-        <div>{{ `v${extension.constants.version}` }}</div>
-        <button @click="settings.methods.toggleDev">
-            {{ settings.states.dev ? 'dev' : 'not dev' }}
-        </button>
+    <div class="m-2 bg-container rounded-lg">
+        <div class="grid grid-flow-row gap-2 p-3">
+            <a
+                class="flex items-center"
+                href="#"
+                @click="
+                    openUrl(
+                        'https://github.com/Internet-Society-Belgium/trest/blob/main/PRIVACY.md'
+                    )
+                "
+            >
+                <div class="flex-none">
+                    <ShieldCheckIcon class="w-5 h-5 text-neutral" />
+                </div>
+                <p class="flex-grow pl-1 pr-3 whitespace-nowrap">
+                    Privacy Policy
+                </p>
+                <div class="flex-none">
+                    <ChevronRightIcon class="w-5 h-5 text-neutral" />
+                </div>
+            </a>
+
+            <a
+                class="flex items-center"
+                href="#"
+                @click="
+                    openUrl(
+                        'https://github.com/Internet-Society-Belgium/trest/blob/main/LICENSE.md'
+                    )
+                "
+            >
+                <div class="flex-none">
+                    <ScaleIcon class="w-5 h-5 text-neutral" />
+                </div>
+                <p class="flex-grow pl-1 pr-3 whitespace-nowrap">License</p>
+                <div class="flex-none">
+                    <ChevronRightIcon class="w-5 h-5 text-neutral" />
+                </div>
+            </a>
+
+            <a
+                class="flex items-center"
+                href="#"
+                @click="
+                    openUrl(
+                        'https://github.com/Internet-Society-Belgium/trest/blob/main/CREDITS.md'
+                    )
+                "
+            >
+                <div class="flex-none">
+                    <UsersIcon class="w-5 h-5 text-neutral" />
+                </div>
+                <p class="flex-grow pl-1 pr-3 whitespace-nowrap">Credits</p>
+                <div class="flex-none">
+                    <ChevronRightIcon class="w-5 h-5 text-neutral" />
+                </div>
+            </a>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
+    import {
+        ChevronRightIcon,
+        ShieldCheckIcon,
+        UsersIcon,
+        ScaleIcon,
+    } from '@heroicons/vue/outline'
     import { defineComponent, inject } from 'vue'
-    import { StoreExtensionKey } from '../types/store/extension'
     import { StoreSettingsKey } from '../types/store/settings'
+    import { openUrl } from '../utils/url'
     export default defineComponent({
         name: 'Settings',
+        components: { ChevronRightIcon, ShieldCheckIcon, UsersIcon, ScaleIcon },
         setup() {
-            const extension = inject(StoreExtensionKey)
             const settings = inject(StoreSettingsKey)
-            return { extension, settings }
+            return { settings, openUrl }
         },
     })
 </script>
