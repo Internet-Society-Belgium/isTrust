e61630814c2d1a2b4e10a0dd34d6505cd643f0b0
Progitiel <github@progiciel.be>
2021-09-15 09:08:10 +0200

    feat: dark colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
     theme: {
         colors: {
             primary: '#0077FF', // HSB(210, 100, 100)
-            secondary: '#000',
-            'dark-secondary': '#f2f4f5',
-            container: '#ffffff',
-            'dark-container': '#252531',
-            'secondary-container': '#E3E4E6', // HSB(210, 1, 90)
-            'secondary-dark-container': '#252531',
-            background: '#F0F1F2', // HSB(210, 1, 95)
-            'dark-background': '#151419',
+            secondary: '#19191A', // HSB(210, 2, 10)
+            'dark-secondary': '#EDF0F2', // HSB(210, 2, 95)
+            container: '#FAFCFF', // HSB(210, 2, 100)
+            'dark-container': '#292E33', // HSB(210, 20, 20)
+            'secondary-container': '#E1E3E6', // HSB(210, 2, 90)
+            'secondary-dark-container': '#3D454D', // HSB(210, 20, 30)
+            background: '#EDF0F2', // HSB(210, 2, 95)
+            'dark-background': '#14171A', // HSB(210, 20, 10)
             ok: '#7ACC29', // HSB(90, 80, 80)
             neutral: '#8A9199', // HSB(210, 10, 60)
             warning: '#FF66B3', // HSB(330, 60, 100)
