7482b657c411e4e0e365ec3a4ff9c01b344fd65d
Progitiel <github@progiciel.be>
2021-09-15 08:51:25 +0200

    feat: change light colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,18 @@ module.exports = {
     darkMode: 'class',
     theme: {
         colors: {
-            primary: '#0072f2',
+            primary: '#0077FF', // HSB(210, 100, 100)
             secondary: '#000',
             'dark-secondary': '#f2f4f5',
             container: '#ffffff',
             'dark-container': '#252531',
-            background: '#f2f4f5',
+            'secondary-container': '#E3E4E6', // HSB(210, 1, 90)
+            'secondary-dark-container': '#252531',
+            background: '#F0F1F2', // HSB(210, 1, 95)
             'dark-background': '#151419',
-            ok: '#7cb72e',
-            neutral: '#89939b',
-            warning: '#f463a4',
+            ok: '#7ACC29', // HSB(90, 80, 80)
+            neutral: '#8A9199', // HSB(210, 10, 60)
+            warning: '#FF66B3', // HSB(330, 60, 100)
         },
         fontFamily: {
             sans: ['Rubik', 'sans-serif'],
