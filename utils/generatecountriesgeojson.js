0e27144d01233f2a21f762705cef7f4956dcda65
Progitiel <github@progiciel.be>
2021-09-20 08:54:18 +0200

    feat(countries): generate based on chapters.ts

--- utils/generatecountriesgeojson.js
+++ utils/generatecountriesgeojson.js
@@ -1,35 +1,86 @@
 const axios = require('axios')
 const path = require('path')
 const fs = require('fs')
+const prettier = require('prettier')
 
-const outputFile = path.resolve(
+const chaptersFile = path.resolve(
+    __dirname,
+    '../src/background/chapters/chapters.ts'
+)
+const countriesFile = path.resolve(
     __dirname,
     '../src/background/chapters/countries.ts'
 )
 
 ;(async () => {
-    const { status, data } = await axios.get(
+    const { status: status110, data: data110 } = await axios.get(
         `https://github.com/nvkelso/natural-earth-vector/raw/master/geojson/ne_110m_admin_0_countries.geojson`
     )
-
-    if (status !== 200) return
-
+    if (status110 !== 200) return
+    const { status: status50, data: data50 } = await axios.get(
+        `https://github.com/nvkelso/natural-earth-vector/raw/master/geojson/ne_50m_admin_0_countries.geojson`
+    )
+    if (status50 !== 200) return
     const cleanedData = {}
 
-    data.features
-        .sort((a, b) => b.properties.POP_RANK - a.properties.POP_RANK)
-        .forEach((f) => {
-            cleanedData[f.properties.NAME] = {
-                bbox: f.bbox,
-                geometry: f.geometry,
-            }
+    const chaptersTs = fs.readFileSync(chaptersFile).toString()
+
+    chaptersRaw = chaptersTs
+        .replace(`import { Chapters } from '../types/chapters'`, '')
+        .replace(`const chapters: Chapters = `, '')
+        .replace(`export default chapters`, '')
+
+    const chapters = JSON.parse(
+        prettier.format(chaptersRaw, {
+            parser: 'json',
         })
+    )
+
+    for (const key in chapters) {
+        if (Object.hasOwnProperty.call(chapters, key)) {
+            let country = data110.features.find(
+                (f) => f.properties.NAME === key
+            )
+            if (!country) {
+                country = data50.features.find((f) => f.properties.NAME === key)
+            }
+            if (!country) throw new Error(`no country ${country}`)
+
+            cleanedData[key] = {
+                POP_RANK: country.properties.POP_RANK,
+                bbox: country.bbox,
+                geometry: country.geometry,
+            }
+        }
+    }
+
+    const cleanedDataOrdered = Object.keys(cleanedData)
+        .sort((a, b) => cleanedData[b].POP_RANK - cleanedData[a].POP_RANK)
+        .reduce((obj, key) => {
+            obj[key] = {
+                bbox: cleanedData[key].bbox,
+                geometry: cleanedData[key].geometry,
+            }
+            return obj
+        }, {})
+
+    const config = await prettier.resolveConfig('.prettierrc.js')
 
     fs.writeFileSync(
-        outputFile,
-        `
-import { Countries } from "../types/countries";
-const countries: Countries = ${JSON.stringify(cleanedData, null, 2)}
-export default countries`
+        countriesFile,
+        prettier.format(
+            `
+        import { Countries } from "../types/countries";
+        const countries: Countries = ${JSON.stringify(
+            cleanedDataOrdered,
+            null,
+            2
+        )}
+        export default countries`,
+            {
+                parser: 'typescript',
+                ...config,
+            }
+        )
     )
 })()
