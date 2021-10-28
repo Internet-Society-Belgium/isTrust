9343aad48c5c2f130e255e1d256795bf73181e4c
Progitiel <github@progiciel.be>
2021-10-28 23:36:18 +0200

    chore(screenshot): fix scripts

--- utils/screenshot/resizeimages.sh
+++ utils/screenshot/resizeimages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -p "screenshots directory: " dir
-read -p "size (1400x600): " size
+read -p "size (1280x800): " size
 for file in $(find $dir -name '*.png'); do 
     convert $file -background 'rgba(0, 0, 0, 0)' -gravity center -extent $size $file
 done
--- utils/screenshot/tempfirefox.sh
+++ utils/screenshot/tempfirefox.sh
@@ -2,4 +2,3 @@
 
 PROFILEDIR=`mktemp -p /tmp -d tmp-fx-profile.XXXXXX.d`
 firefox -profile $PROFILEDIR -no-remote -new-instance --new-tab about:preferences
-rm -rf $PROFILEDIR
