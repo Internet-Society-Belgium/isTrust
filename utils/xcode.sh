b4d299be2f6fc5fffbd6d043681b81e33fe9b858
Progitiel <github@progiciel.be>
2021-12-15 23:32:57 +0100

    build: xcode instruction
    #93

--- utils/xcode.sh
+++ utils/xcode.sh
@@ -4,6 +4,10 @@ rm -rf safari/
 xcrun safari-web-extension-converter $PWD/build --project-location $PWD/safari --app-name isTrust --bundle-identifier be.isoc.istrust --swift --macos-only --copy-resources
 echo "Go to isTrust > Target > isTrust"
 echo "App Category: Utilities"
+echo "Bundle Identifier: be.isoc.istrust"
 echo "Version: ..."
 echo "Go to isTrust > Target > isTrust Extension"
+echo "Bundle Identifier: be.isoc.istrust.extension"
 echo "Version: ..."
+echo "Go to Product > Archive"
+echo "Distribute App (Window > Organize)"
