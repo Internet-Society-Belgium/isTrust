7c55ab33909736a72c313744235b27e44f2f8e51
Progitiel <github@progiciel.be>
2021-12-15 22:03:41 +0100

    build: improve build:safari
    #93

--- utils/xcode.sh
+++ utils/xcode.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-npm run build
 rm -rf safari/
-xcrun safari-web-extension-converter $PWD/build --project-location $PWD/safari --app-name isTrust --bundle-identifier be.isoc.istrust --swift --macos-only --copy-resources --force
+xcrun safari-web-extension-converter $PWD/build --project-location $PWD/safari --app-name isTrust --bundle-identifier be.isoc.istrust --swift --macos-only --copy-resources
 echo "Go to isTrust > Target > isTrust"
 echo "App Category: Utilities"
 echo "Version: ..."
