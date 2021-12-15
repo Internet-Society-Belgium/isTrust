292efffe6b11a181cbae3adfc45bf28d98823640
Progitiel <github@progiciel.be>
2021-12-15 21:55:24 +0100

    build: xcode utils script
    close #93

---
+++ utils/xcode.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+npm run build
+rm -rf safari/
+xcrun safari-web-extension-converter $PWD/build --project-location $PWD/safari --app-name isTrust --bundle-identifier be.isoc.istrust --swift --macos-only --copy-resources --force
+echo "Go to isTrust > Target > isTrust"
+echo "App Category: Utilities"
+echo "Version: ..."
+echo "Go to isTrust > Target > isTrust Extension"
+echo "Version: ..."
