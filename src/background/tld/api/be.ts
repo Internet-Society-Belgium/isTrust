3b9b2f640cadcf748a4ceaa911ac28a01c089271
Progitiel <github@progiciel.be>
2021-12-30 10:11:34 +0100

    feat(api/be): onsite contact

--- src/background/tld/api/be.ts
+++ src/background/tld/api/be.ts
@@ -33,10 +33,22 @@ export default class Website_be extends Website {
                 dnssec: registration.dnsKeyInfo.dnsKeys.length !== 0,
             }
 
+            let contact: string | null = null
+
             if (registration.registrant) {
+                contact = registration.registrant
+            } else if (
+                registration.onsiteCompanyContacts &&
+                Array.isArray(registration.onsiteCompanyContacts) &&
+                registration.onsiteCompanyContacts.length > 0
+            ) {
+                contact = registration.onsiteCompanyContacts[0]
+            }
+
+            if (contact) {
                 const { status: registrantStatus, data: registrant } =
                     await axios.get<DnsBelgiumContact>(
-                        `https://api.dnsbelgium.be/whois/contact/${registration.registrant}`
+                        `https://api.dnsbelgium.be/whois/contact/${contact}`
                     )
 
                 if (registrantStatus === 200) {
--- src/background/types/dnsbelgium.ts
+++ src/background/types/dnsbelgium.ts
@@ -4,6 +4,7 @@ export interface DnsBelgiumRegistration {
         updated: string
     }
     registrant: string | null
+    onsiteCompanyContacts: string[]
     dnsKeyInfo: {
         dnsKeys: []
     }
