e02afcb55a3ac6d83531bfd82dd16f83854b333d
Progitiel <github@progiciel.be>
2022-05-17 00:03:53 +0200

    docs(xcode): improve steps

--- utils/xcode.sh
+++ utils/xcode.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# if PATH error: sudo xcode-select -switch /Applications/Xcode.app
+
 rm -rf safari/
 xcrun safari-web-extension-converter $PWD/build --project-location $PWD/safari --app-name isTrust --bundle-identifier be.isoc.istrust --swift --macos-only --copy-resources
 echo "Go to isTrust > Target > isTrust"
@@ -7,11 +9,13 @@ echo "App Category: Utilities"
 echo "Bundle Identifier: be.isoc.istrust"
 echo "Version: ..."
 echo "Go to ... > Signing & Capabilities"
+echo "Team: Internet Society Chapter Begium"
 echo "Remove unused permission"
 echo "Go to isTrust > Target > isTrust Extension"
 echo "Bundle Identifier: be.isoc.istrust.extension"
 echo "Version: ..."
 echo "Go to ... > Signing & Capabilities"
+echo "Team: Internet Society Chapter Begium"
 echo "Remove unused permission"
 echo "Go to Product > Archive"
 echo "Distribute App (Window > Organize)"
