61c9d3f9814ed36682de474c5fa96d76d69bf497
etoome <git@etoo.me>
2025-07-08 16:28:39 +0200

    webextension

---
+++ apps/webextension/src/entrypoints/background/history.ts
@@ -0,0 +1,53 @@
+import { browser } from "#imports";
+
+export interface HistoryData {
+  domain: string;
+  data: {
+    visits: number;
+    firstVisit?: number;
+  };
+}
+
+export async function history({
+  domain,
+}: {
+  domain: string;
+}): Promise<HistoryData> {
+  const historyItems = await browser.history.search({ text: `${domain}` });
+
+  let firstVisit;
+  let visits = 0;
+
+  for (const historyItem of historyItems) {
+    const pageUrl = historyItem.url;
+    if (!pageUrl) continue;
+
+    const pageDomain = new URL(pageUrl).hostname;
+    if (pageDomain !== domain) continue;
+
+    const visitItems = await browser.history.getVisits({ url: pageUrl });
+    for (const visitItem of visitItems) {
+      // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/history/TransitionType
+      if (
+        visitItem.transition === "link" ||
+        visitItem.transition === "typed" ||
+        visitItem.transition === "auto_bookmark" ||
+        visitItem.transition === "generated" ||
+        visitItem.transition === "keyword" ||
+        visitItem.transition === "keyword_generated"
+      ) {
+        visits += 1;
+
+        if (visitItem.visitTime) {
+          const visitTime = new Date(visitItem.visitTime).getTime();
+
+          if (firstVisit === undefined || visitTime < firstVisit) {
+            firstVisit = visitTime;
+          }
+        }
+      }
+    }
+  }
+
+  return { domain, data: { visits, firstVisit } };
+}
