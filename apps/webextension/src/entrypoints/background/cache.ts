f457ac6739f99dbe530c0df1f0b41a812f8be895
etoome <98775952+etoome@users.noreply.github.com>
2025-07-13 14:12:15 +0200

    feat(webextension): whois

---
+++ apps/webextension/src/entrypoints/background/cache.ts
@@ -0,0 +1,31 @@
+import * as common from "@istrust/common";
+import { storage } from "#imports";
+
+export const cache: common.InternalCache = {
+  psl: {
+    set: async (key: string, value: string) =>
+      storage.setItem(`local:psl:${key}`, value),
+    get: async (key: string) => storage.getItem(`local:psl:${key}`),
+    clear: async () => {
+      const snapshot = await storage.snapshot("local");
+      for (const item in snapshot) {
+        if (item.startsWith("psl:")) {
+          storage.removeItem(`local:${item}`);
+        }
+      }
+    },
+  },
+  rdap: {
+    set: async (key: string, value: string) =>
+      storage.setItem(`local:rdap:${key}`, value),
+    get: async (key: string) => storage.getItem(`local:rdap:${key}`),
+    clear: async () => {
+      const snapshot = await storage.snapshot("local");
+      for (const item in snapshot) {
+        if (item.startsWith("rdap:")) {
+          storage.removeItem(`local:${item}`);
+        }
+      }
+    },
+  },
+};
--- apps/webextension/src/entrypoints/background/index.ts
+++ apps/webextension/src/entrypoints/background/index.ts
@@ -1,27 +1,34 @@
 import { onMessage } from "@/utils/messaging";
-import { history } from "./history";
 import { browser, defineBackground } from "#imports";
+import { cache } from "./cache";
+import * as common from "@istrust/common";
 
 export default defineBackground(() => {
-  onMessage("history", async ({ data: { domain } }) => {
-    return await history({
-      domain,
-    });
-  });
-
   browser.contextMenus.create({
     id: "istrust",
     title: "isTrust",
-    contexts: ["link"], // TODO email
+    contexts: ["link"],
   });
   browser.contextMenus.onClicked.addListener(async (info) => {
-    const url = info?.linkUrl;
-    if (!url) return;
+    const text = info?.linkUrl;
+    if (!text) return;
 
     await browser.windows.create({
-      url: `${browser.runtime.getURL("/popup.html")}?url=${url}`,
+      url: `${browser.runtime.getURL("/popup.html")}?q=${text}`,
       type: "popup",
       focused: true,
     });
+  });
+
+  browser.runtime.onInstalled.addListener(async () => {
+    return await common.update_cache(cache);
+  });
+
+  onMessage("whois", async ({ data: { domain } }) => {
+    return await common.whois(domain, cache);
+  });
+
+  onMessage("force_update_cache", async () => {
+    return await common.force_update_cache(cache);
   });
 });
