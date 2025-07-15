52354c82ce41ed54966535c8fed2b5bf9df35d0e
etoome <98775952+etoome@users.noreply.github.com>
2025-07-15 16:23:08 +0200

    143 whois individual (#144)
    * feat(whois): individual
    
    * fix(rdap): prevent infinite loop
    
    * feat(whois): expiration
    
    * refactor: error management
    
    * test(whois): expiration
    
    * ci: only affected
    
    * ci: try fix turbo --affected

--- packages/common/src/whois/type.ts
+++ packages/common/src/whois/type.ts
@@ -38,7 +38,6 @@ const jCardSchema = z.tuple([
 
 // https://datatracker.ietf.org/doc/rfc7483/
 const RdapResultSchema = z.object({
-  ldhName: z.string(),
   events: z.array(
     z.object({
       eventAction: z.string(),
@@ -76,8 +75,10 @@ export function validateRdapResult(json: unknown) {
 
 export interface WHOISData {
   registration?: string;
+  expiration?: string;
   registrant?: {
     organization?: string;
+    individual?: string;
     country?: {
       code?: string;
       name?: string;
