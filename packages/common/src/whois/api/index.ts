6928bbf4acc5694094764029333a1b20de9ab003
etoome <98775952+etoome@users.noreply.github.com>
2025-08-04 20:34:12 +0200

    feat: sidn

--- packages/common/src/whois/api/index.ts
+++ packages/common/src/whois/api/index.ts
@@ -1,5 +1,6 @@
 import { user_error } from "../../utils/error";
 import * as be from "./be";
+import * as nl from "./nl";
 
 export async function get_data(domain: string) {
   const tld = domain.split(".").at(-1);
@@ -7,6 +8,8 @@ export async function get_data(domain: string) {
 
   if (tld === "be") {
     return await be.get_data(domain);
+  } else if (tld === "nl") {
+    return await nl.get_data(domain);
   }
 
   return;
