c4f8d55087b0cd573df2026770688a90d8b68a12
etoome <98775952+etoome@users.noreply.github.com>
2025-08-14 21:32:34 +0200

    build(xcode): bak

--- apps/webextension/scripts/xcode.sh
+++ apps/webextension/scripts/xcode.sh
@@ -8,13 +8,13 @@ rm -rf ./xcode
 xcrun safari-web-extension-converter --project-location xcode --app-name isTrust --bundle-identifier be.isoc.istrust --copy-resources safari-mv2
 
 version=$(npm pkg get version --workspaces=false | tr -d \")
-sed -i -e "s/MARKETING_VERSION = 1.0;/MARKETING_VERSION = $version;/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
-sed -i -e "s/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.isTrust;/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.istrust;/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
-sed -i -e "s/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.isTrust.Extension;/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.istrust.extension;/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
-sed -i -e "s/INFOPLIST_KEY_CFBundleDisplayName = isTrust;/INFOPLIST_KEY_CFBundleDisplayName = isTrust;\nINFOPLIST_KEY_LSApplicationCategoryType = \"public.app-category.utilities\";/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
+sed -i.bak -e "s/MARKETING_VERSION = 1.0;/MARKETING_VERSION = $version;/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
+sed -i.bak -e "s/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.isTrust;/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.istrust;/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
+sed -i.bak -e "s/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.isTrust.Extension;/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.istrust.extension;/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
+sed -i.bak -e "s/INFOPLIST_KEY_CFBundleDisplayName = isTrust;/INFOPLIST_KEY_CFBundleDisplayName = isTrust;\nINFOPLIST_KEY_LSApplicationCategoryType = \"public.app-category.utilities\";/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
 
-sed -i -e "s/<dict>/<dict>\n<key>ITSAppUsesNonExemptEncryption<\/key>\n<false\/>/g" "./xcode/isTrust/macOS (App)/Info.plist"
-sed -i -e "s/<dict>/<dict>\n<key>ITSAppUsesNonExemptEncryption<\/key>\n<false\/>/g" "./xcode/isTrust/iOS (App)/Info.plist"
+sed -i.bak -e "s/<dict>/<dict>\n<key>ITSAppUsesNonExemptEncryption<\/key>\n<false\/>/g" "./xcode/isTrust/macOS (App)/Info.plist"
+sed -i.bak -e "s/<dict>/<dict>\n<key>ITSAppUsesNonExemptEncryption<\/key>\n<false\/>/g" "./xcode/isTrust/iOS (App)/Info.plist"
 
 echo "----------"
 echo "Go to isTrust (file tree root)"
