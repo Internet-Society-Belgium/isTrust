1ddac7ccfcae9f6565f2f8ac26b8a3ad18a23e46
etoome <98775952+etoome@users.noreply.github.com>
2025-08-14 20:23:22 +0200

    build(xcode): settings

--- apps/webextension/scripts/xcode.sh
+++ apps/webextension/scripts/xcode.sh
@@ -7,24 +7,29 @@ rm -rf ./xcode
 
 xcrun safari-web-extension-converter --project-location xcode --app-name isTrust --bundle-identifier be.isoc.istrust --copy-resources safari-mv2
 
-echo ""
+version=$(npm pkg get version --workspaces=false | tr -d \")
+sed -i -e "s/MARKETING_VERSION = 1.0;/MARKETING_VERSION = $version;/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
+sed -i -e "s/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.isTrust;/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.istrust;/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
+sed -i -e "s/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.isTrust.Extension;/PRODUCT_BUNDLE_IDENTIFIER = be.isoc.istrust.extension;/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
+sed -i -e "s/INFOPLIST_KEY_CFBundleDisplayName = isTrust;/INFOPLIST_KEY_CFBundleDisplayName = isTrust;\nINFOPLIST_KEY_LSApplicationCategoryType = \"public.app-category.utilities\";/g" ./xcode/isTrust/isTrust.xcodeproj/project.pbxproj
+
+sed -i -e "s/<dict>/<dict>\n<key>ITSAppUsesNonExemptEncryption<\/key>\n<false\/>/g" "./xcode/isTrust/macOS (App)/Info.plist"
+sed -i -e "s/<dict>/<dict>\n<key>ITSAppUsesNonExemptEncryption<\/key>\n<false\/>/g" "./xcode/isTrust/iOS (App)/Info.plist"
+
+echo "----------"
 echo "Go to isTrust (file tree root)"
 echo "----- App -----"
 echo "Go to isTrust (...)"
-echo "App Category: Utilities"
-echo "Bundle Identifier: be.isoc.istrust"
-echo "Version: ..."
+# echo "App Category: Utilities"
+# echo "Bundle Identifier: be.isoc.istrust"
+# echo "Version: ..."
 echo "Go to Signing & Capabilities"
 echo "Team: Internet Society Chapter Begium"
 echo "Remove unused permission"
 echo "----- Extension -----"
 echo "Go to isTrust Extension (...)"
-echo "Bundle Identifier: be.isoc.istrust.extension"
-echo "Version: ..."
+# echo "Bundle Identifier: be.isoc.istrust.extension"
+# echo "Version: ..."
 echo "Go to Signing & Capabilities"
 echo "Team: Internet Society Chapter Begium"
 echo "Remove unused permission"
-echo "----- Archive -----"
-echo "Go to Product > Archive"
-echo "Validate App (Window > Organize)"
-echo "Distribute App (Window > Organize)"
