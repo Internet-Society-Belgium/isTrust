eb92394b8e4eec431656d9752e58faa3c45ab983
etoome <98775952+etoome@users.noreply.github.com>
2025-08-20 15:21:57 +0200

    fix: lint

--- configs/eslint/typescript.js
+++ configs/eslint/typescript.js
@@ -13,34 +13,13 @@ export const config = {
       projectService: true,
       tsconfigRootDir: import.meta.dirname,
     },
+    globals: {
+      console: false,
+    },
   },
   rules: {
     eqeqeq: ["error", "always"],
     "@typescript-eslint/strict-boolean-expressions": "error",
-    // "@typescript-eslint/naming-convention": [
-    //   "warn",
-    //   {
-    //     selector: "default",
-    //     format: ["snake_case"],
-    //   },
-    //   {
-    //     selector: "variableLike",
-    //     format: ["snake_case", "UPPER_CASE"],
-    //   },
-    //   {
-    //     selector: "variable",
-    //     types: ["function"],
-    //     format: ["StrictPascalCase"],
-    //   },
-    //   {
-    //     selector: "function",
-    //     format: ["StrictPascalCase"],
-    //   },
-    //   {
-    //     selector: "typeLike",
-    //     format: ["StrictPascalCase"],
-    //   },
-    // ],
   },
 };
 
