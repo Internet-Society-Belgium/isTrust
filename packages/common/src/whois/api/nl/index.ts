099831a81aaf3431f1f53817d45f0010aa0c3159
etoome <98775952+etoome@users.noreply.github.com>
2025-12-06 11:39:27 +0100

    fix(sidn.nl): api

--- packages/common/src/whois/api/nl/index.ts
+++ packages/common/src/whois/api/nl/index.ts
@@ -23,7 +23,7 @@ export async function get_data(domain: string) {
   ];
 
   try {
-    const res = await fetch(`https://api.sidn.nl/rest/whois?domain=${domain}`);
+    const res = await fetch(`https://api.sidn.nl/rdap/whois?domain=${domain}`);
 
     if (!res.ok) throw source_error("No registration response");
 
